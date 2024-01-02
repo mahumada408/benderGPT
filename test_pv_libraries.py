12d84a7fa81452e41097ec00999e7c8945168589
Manuel Ahumada <mahumada90@gmail.com>
2024-01-01 19:28:18 -0800

    working mic demo with transcription

--- test_pv_libraries.py
+++ test_pv_libraries.py
@@ -1,9 +1,15 @@
+import os
 import pvporcupine, pvcheetah
 from pvrecorder import PvRecorder
 
 ENDPOINT_DURATION_SECONDS = 2 # 'Quiet' seconds indicating the end of audio capture
-
+AUDIO_DEVICE = "Plugable USB Audio Device Analog Stereo"
+MIC_INDEX = 3
 porcupine_key = os.environ.get("PORCUPINE_API_KEY")
+
+porcupine = pvporcupine.create(
+    access_key=porcupine_key,
+    keywords=['terminator'])
 
 cheetah = pvcheetah.create(
     access_key=porcupine_key,
@@ -11,8 +17,8 @@ cheetah = pvcheetah.create(
     enable_automatic_punctuation=True)
 
 recorder = PvRecorder(
-    frame_length=porcupine.frame_length,
-    device_index=AUDIO_DEVICE)
+    frame_length=512,
+    device_index=MIC_INDEX)
 
 # Speech-to-text using Picovoice's Cheetah
 def capture_input():
@@ -30,7 +36,14 @@ def main():
 
     recorder.start()
 
-    script = capture_input()
+    while True:
+        pcm = recorder.read()
+        result = porcupine.process(pcm)
+
+        if result >= 0:
+            print('keyword detected')
+            script = capture_input()
+            print(script)
 
 if __name__ == '__main__':
     main()
